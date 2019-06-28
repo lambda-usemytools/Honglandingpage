330a73c9f600968122f6e17e2bfe3ff29234596b
Your Name <you@example.com>
2019-06-28 00:14:11 -0400

    added Event Mouseout for nav and buttons at Landing page

--- JS/Tabs.js
+++ JS/Tabs.js
@@ -1,27 +1,57 @@
 
-function mouseHandler(){
-    event.target.style.color = "brown";
-};
+const navLive = document.querySelectorAll("nav a");
 
-const navStyle = document.querySelectorAll("nav a");
-
-navStyle.forEach((link)=>{
-    link.addEventListener("mouseover",mouseHandler);
+navLive.forEach((event)=>{
+    event.addEventListener("mouseover",(event)=>{
+        event.target.style.color = "brown"
+    });
 })
+
+navLive.forEach((event)=>{
+    event.addEventListener("mouseout",(event)=>{
+        event.target.style.color = "#1E3A46"
+    });
+})
+
+const titleLive = document.querySelectorAll(".title");
+
+titleLive.forEach((event)=>{
+    event.addEventListener("mouseover",(event)=>{
+        event.target.style.color = "brown"
+    });
+})
+
+titleLive.forEach((event)=>{
+    event.addEventListener("mouseout",(event)=>{
+        event.target.style.color = "#41340C"
+    });
+})
+
 
 function mouseHandlerButton(){
     event.target.style.backgroundColor = "brown";
+    event.target.style.color = "#EDE3C2"
 }
 
 const firstButton = document.querySelector(".btn");
 
-firstButton.addEventListener("mousemove",mouseHandlerButton);
+firstButton.addEventListener("mouseover",mouseHandlerButton);
+firstButton.addEventListener("mouseout",(event)=>{
+    event.target.style.backgroundColor = " #488EAC"
+});
 
 const secondButton = document.querySelector(".btn-2");
 
 secondButton.addEventListener("mousemove", mouseHandlerButton);
+secondButton.addEventListener("mouseout",(event)=>{
+    event.target.style.backgroundColor = "#DAAB1D"
+    event.target.style.color = "#336277"
+});
 
 const thirdButton = document.querySelector(".btn-3");
 
 thirdButton.addEventListener("mousemove",mouseHandlerButton);
+thirdButton.addEventListener("mouseout",(event)=>{
+    event.target.style.backgroundColor = "#488EAC"
+});
 
